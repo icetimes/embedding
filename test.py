e29e7ef0edbf448fe3b83d95b3b1aab77c5cb337
huyulan <huyulan@boe.com.cn>
2018-11-26 10:45:13 +0800

    add test

---
+++ test.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2018/11/26 10:40
+# @Author  : huyulan
+# @Email   : huyulan@boe.com.cn
+# @File    : test.py
+# @Software: PyCharm
+print("my name is huyulan.")
